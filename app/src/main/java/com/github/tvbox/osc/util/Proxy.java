8efcd167e588715b1fc1b4ad37a41092cb17355c
sayxiaoyan <127412316+sayxiaoyan@users.noreply.github.com>
2025-03-24 23:39:38 +0800

    Create Proxy.java

---
+++ app/src/main/java/com/github/tvbox/osc/util/Proxy.java
@@ -0,0 +1,21 @@
+package com.github.tvbox.osc.util;
+
+import java.util.Map; 
+import com.github.tvbox.osc.util.parser.SuperParse;
+public class Proxy {
+
+    public static Object[] proxy(Map<String, String> params) {
+        try {
+            String what = params.get("go");
+            assert what != null;
+            if ("SuperParse".equals(what)) {
+                return SuperParse.loadHtml(params.get("flag"), params.get("url"));
+            }
+
+        } catch (Throwable ignored) {
+
+        }
+        return null;
+    }
+
+}
